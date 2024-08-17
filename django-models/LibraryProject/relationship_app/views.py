780dd3db2e3aa7482d16a72b81c0dff2ca14eefd
Evans Torddey <ea.torddey@gmail.com>
2024-08-17 19:32:24 +0000

    add views & urls

---
+++ django-models/LibraryProject/relationship_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Book
+from django.views.generic import DetailView  
+from .models import Library
+# Create your views here.
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/Library_detail.html'
