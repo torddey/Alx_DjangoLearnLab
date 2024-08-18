f5ec7130e548813e53a1b2b46a3919e1f84fda1b
Evans Torddey <ea.torddey@gmail.com>
2024-08-18 20:04:29 +0000

    'update'

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Book
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from .models import Library
 
 # Create your views here.
