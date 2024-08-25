4ce2319886d884a2d547163d349b21b5630eef42
Evans Torddey <ea.torddey@gmail.com>
2024-08-25 22:12:41 +0000

    perms

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -30,3 +30,11 @@ class CustomUserManager(BaseUserManager):
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    class Meta:
+        permissions = [
+            ("create", "can create Posts"),
+            ("view", "can view Books"),
+            ("edit", "can edit Books"),
+            ("delete", "can delete Books")
+        ]
