362d2db6e4e4a62df52e91fbed75011b4ccd92e2
Evans Torddey <ea.torddey@gmail.com>
2024-08-30 12:57:58 +0000

    Updates

---
+++ api_project/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import BookListCreateAPIView
+
+
+urlpatterns = [
+    path(
+        "books/", 
+        BookListCreateAPIView.as_view(), 
+        name="book_list_create_view",
+    )
+]
