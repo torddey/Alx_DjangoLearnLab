077e433049ba13c7d4efe9ab2a8b2069a5d8c966
Evans Torddey <ea.torddey@gmail.com>
2024-09-09 23:38:07 +0000

    update

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,6 +4,7 @@ from .serializers import BookSerializer
 from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.filters import OrderingFilter, SearchFilter
+from django_filters import rest_framework
 from django_filters.rest_framework import DjangoFilterBackend
 
 
