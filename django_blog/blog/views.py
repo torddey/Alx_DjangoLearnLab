16d2e77dfbfeada1cd737f96137925e79a22c070
Evans Torddey <ea.torddey@gmail.com>
2024-09-11 17:00:41 +0000

    Configure Views & Urls

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def register(request):
+    if request.moethod == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+    return render(request, "register.html", {"form":form})
+
+class LoginView(LoginView):
+    template_name = "login.html"
+
+def profile(request):
+    return render(request, "profile.html")
+
+def base(request):
+    return render(request, "base.html")
+
+def blog_posts(request):
+    posts = Post.objects.all()
+    return render(request, "blog/blog_posts.html", {"posts":posts})
+
