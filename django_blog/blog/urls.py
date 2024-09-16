db71964f7a09a9ee7139f5aa05467b2d9bb530f3
Evans Torddey <ea.torddey@gmail.com>
2024-09-16 20:13:50 +0000

    Update routes

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import base, register, LoginView, profile, PostListView
+from .views import home, register, LoginView, profile, PostListView
 from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
-    path("", base, name="base"),
+    path("", home, name="home"),
     path("register/", register, name="register"),
-    path("login/", LoginView.as_view(next_page="base"), name="login"),
+    path("login/", LoginView.as_view(next_page="profile"), name="login"),
     path("profile/", profile, name="profile"),
     path("logout/", LogoutView.as_view(next_page="/"), name="logout"),
     path("posts/", PostListView.as_view, name="posts")
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -8,7 +8,7 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from .models import Post
-from rest_framework import generics, filters
+
 
 
 
@@ -24,63 +24,17 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'register.html', {'form': form})
 
-@login_required
-def profile(request):
-    return render(request, 'blog/profile.html')
-
 class LoginView(LoginView):
     template_name = "login.html"
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
+
+def home(request):
+    return render(request, "base.html")
 
 class PostListView(ListView):
     model = Post
     template_name = "posts.html"
-    # content_object_name = 'posts'
-
-def list_post(request):
-    posts = Post.objects.all()
-    return render(request, 'posts.html')
-
-
-
-def base(request):
-    return render(request, "base.html")
-
-
-#List all Books by filtering, search and ordering.
-class PostListView(generics.ListAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    filterset_fields = ['title', 'author', 'publication_year'] #Fields to filter
-    search_fields = ['title', 'author'] # Specific search fields
-    ordering_fields = ['title', 'publication_year'] # Order fields
-    ordering = ['title']  # default ordering
-
-
-#Create new Book. Only authenticated users can create new books.
-class PostCreateView(generics.CreateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated]
-
-
-#Get details of Books. Any user can retrieve book details
-class PostDetailView(generics.RetrieveAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
-
-
-#Update a book by ID. Only authenticated users can execute this 
-class PostUpdateView(generics.UpdateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated]
-
-
-#Onlu authenticated users can Destroy book 
-class PostDeleteView(generics.DestroyAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated]
+    content_object_name = 'posts'
