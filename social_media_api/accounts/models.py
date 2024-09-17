7a0fe4a8976392663b9d7cfb877a0a5c1f7ed162
Evans Torddey <ea.torddey@gmail.com>
2024-09-17 20:27:08 +0000

    Setup

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     bio = models.TextField(max_length=500, blank=True)
-    profile_picture = models.URLField(max_length=200, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
     followers = models.ManyToManyField('self', symmetrical=False, related_name='following')
 
     def __str__(self):
